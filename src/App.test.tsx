d3d2a6d9363ef0be0f7e798d3253e54fcdb25730
shreyya1208 <shreya.singour.6@gmail.com>
2025-08-10 18:14:29 +0530

    Updated App.test.tsx to match new application structure

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { BrowserRouter } from "react-router-dom";
+
+test("renders the main application layout", () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+
+  const headerElement = screen.getByText(/Form Builder/i);
+  expect(headerElement).toBeInTheDocument();
+
+  const createLink = screen.getByText(/Create/i);
+  expect(createLink).toBeInTheDocument();
 });
